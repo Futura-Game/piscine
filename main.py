647881447cacd68267e63d6e7270a369fd8e3d46
Futura_Game <thomas.joriatti@orange.fr>
2025-01-25 22:08:15 +0100

    Ajout de la date

--- main.py
+++ main.py
@@ -1,247 +1,189 @@
 import os
+from datetime import datetime
 
-commande = ''
-
-#default values
-param = {'bdd': [(1,3,10),(2,1,13),(3,2,6), (3,1,8) ],
+# Default values
+param = {'bdd': [(1, 3, 10, '2024-05-11'), (2, 1, 13, '2024-05-12'), (3, 2, 6, '2024-05-11'), (3, 1, 8, '2024-05-13')],
          'nages': [(1, "Brasse"), (2, "Dos"), (3, "Crawl")],
          'nageurs': [(1, "Pierre"), (2, "Paul"), (3, "Léa")]
-        }
+         }
 
 
 def reset(param):
-    '''réinitialise la bdd'''
+    """Réinitialise la base de données."""
     param.clear()
     param['bdd'] = []
     param['nages'] = []
     param['nageurs'] = []
 
 
-def get_str_from_num_in_list(num, liste):
-    """Return str from num into liste"""
-    for elt in liste:
-        if elt[0]==num:
-            return elt[1]
-    #la ligne suivante ne devrait jamais être exécutée
-    return "unknown"
-
-
-def cmd_individu(param):
-    """Ajoute un nouveau najeur"""
-    prénom = input("Prénom du nouveau nageur ? ")
-    id = len(param['nageurs'])+1
-    param['nageurs'].append( (id,prénom ))
-    print(param['nageurs'])
-
-
-def cmd_nouvelle_nage(param):
-    """Ajoute une nouvelle nage au logiciel"""
-    nage = input("Quelle nage enregistrer ? ")
-    id = len(param['nages'])+1
-    param['nages'].append( (id,nage ))
-    print(param['nages'])
-
-
-def cmd_ajout(param):
-    """Ajoute un évènement à la liste"""
-    for elt in param['nageurs']:
-        print(f"{elt[0]:5} : {elt[1]}")
-    a = int(input("Nageur n° ? "))
-    for elt in param['nages']:
-        print(f"{elt[0]:5} : {elt[1]}")
-    b = int(input("Nage n° ? "))
-    c = int(input("combien de longueur ? "))
-    param['bdd'].append((a,b,c))
-
-
-def cmd_liste(param):
-    """Affiche toutes les performances des nageurs"""
-    print("Prénom      |  nage   |  longueur")
-    print("---------------------------------")
-    for elt in param['bdd']:
-        nageur = get_str_from_num_in_list(elt[0], param['nageurs'])
-        nage = get_str_from_num_in_list(elt[1], param['nages'])
-        print(f" {nageur:11}| {nage:8}|  {elt[2]}")
-
-
-def cmd_nageur(param):
-    """Affiche toutes les performances d'un nageur"""
-    for elt in param['nageurs']:
-        print(f"{elt[0]:5} : {elt[1]}")
-    tmp = int(input("Quel numéro de nageur ? "))
-    print("Performances de ", tmp)
-    print("  nage   |  longueur")
-    print("--------------------")
-    for elt in param['bdd']:
-        if elt[0]== tmp:
-            nage = get_str_from_num_in_list(elt[1], param['nages'])
-            print(f" {nage:8}|  {elt[2]}")
-
-
-def cmd_nage(param):
-    """Affiche toutes les performances suivant une nage donnée"""
-    for elt in param['nages']:
-        print(f"{elt[0]:5} : {elt[1]}")
-    tmp = int(input("Quel numéro de nage ? "))
-    print("Nage ", tmp)
-    print(" Nageur     |  longueur")
-    print("------------------------")
-    for elt in param['bdd']:
-        if elt[1]== tmp:
-            nageur = get_str_from_num_in_list(elt[0], param['nageurs'])
-            print(f" {nageur:11}|  {elt[2]}")
-
-
-def cmd_exit(param):
-    tmp = input("En êtes-vous sûr ? (o)ui/(n)on ")
-    if tmp == 'o':
-        cmd_save(param, 'save.backup')
-        return False
-    else:
-        return True
-
-
-def cmd_save(param, filename = 'save.csv'):
-    '''sauvegarde complète de la BDD'''
-    fichier = open(filename, 'w')
-    # sauvegarde des nageurs
-    fichier.write('@ nageurs\n')
-    for elt in param['nageurs']:
-        fichier.write(str(elt[0])+','+str(elt[1])+"\n")
-    # sauvegarde des nages
-    fichier.write('@ nages\n')
-    for elt in param['nages']:
-        fichier.write(str(elt[0])+','+str(elt[1])+"\n")
-    # sauvegarde des données
-    fichier.write('@ bdd\n')
-    for elt in param['bdd']:
-        fichier.write(str(elt[0])+','+str(elt[1])+','+str(elt[2])+"\n")
-    fichier.close()
-
-
-def cmd_load(param, filename = 'save.csv'):
-    '''chargement complet la BDD avec réinitialisation'''
-    reset(param)
-    key = ''
-    fichier = open(filename, 'r')
-    for line in fichier:
-        line.strip()
-        if line[-1] == '\n':
-            line = line[:-1]
-        if line[0]=='#':
-            continue
-        if line[0]=='@':
-            key = line[2:]
-            continue
-        if key =='':
-            continue
-        tmp = line.split(',')
-        # convertion en int de ce qui doit l'être
-        if key == 'bdd':
-            for i in range(len(tmp)):
-                tmp[i] = int(tmp[i])
-        if key == 'nages' or key == 'nageurs':
-            tmp[0] = int(tmp[0])
-        param[key].append(tuple(tmp))
-    fichier.close()
-
-
-def get_cmd():
-    '''Traitement de la commande d'entrée'''
-    msg = input("Que faut-il faire ? ")
-    msg = msg.lower()
-    return msg
-
 def afficher_menu():
-    print("Choisissez une option :")
+    print("\nChoisissez une option :")
     print("1 -> Ajout d'une performance")
     print("2 -> Ajout d'un individu")
     print("3 -> Ajout d'une nouvelle nage")
     print("4 -> Liste toutes les performances")
     print("5 -> Liste les performances d'un nageur")
     print("6 -> Liste tous les nageurs pratiquant une nage")
-    print("7 -> Sauvegarde les données utilisateurs")
-    print("8 -> Charge les données utilisateurs")
+    print("7 -> Liste des performances d'un jour spécifique")
+    print("8 -> Sauvegarde les données utilisateurs")
+    print("9 -> Charge les données utilisateurs")
     print("0 -> Quitte le logiciel")
 
-def gestion_commande():
+
+def get_str_from_num_in_list(num, liste):
+    """Retourne la valeur associée à un ID dans une liste."""
+    for elt in liste:
+        if elt[0] == num:
+            return elt[1]
+    return "unknown"
+
+
+def cmd_ajout(param):
+    """Ajoute une performance."""
+    for elt in param['nageurs']:
+        print(f"{elt[0]:5} : {elt[1]}")
+    a = int(input("Nageur n° ? "))
+    for elt in param['nages']:
+        print(f"{elt[0]:5} : {elt[1]}")
+    b = int(input("Nage n° ? "))
+    c = int(input("Combien de longueurs ? "))
+    date = input("Date de l'événement (YYYY-MM-DD) ? ")
+    # Validation du format de la date
+    try:
+        datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        print("Date invalide. Utilisez le format YYYY-MM-DD.")
+        return
+    param['bdd'].append((a, b, c, date))
+    print("Performance ajoutée avec succès.")
+
+
+def cmd_individu(param):
+    """Ajoute un nouvel individu."""
+    prénom = input("Prénom du nouveau nageur ? ")
+    id = len(param['nageurs']) + 1
+    param['nageurs'].append((id, prénom))
+    print(param['nageurs'])
+
+
+def cmd_nouvelle_nage(param):
+    """Ajoute une nouvelle nage."""
+    nage = input("Quelle nage enregistrer ? ")
+    id = len(param['nages']) + 1
+    param['nages'].append((id, nage))
+    print(param['nages'])
+
+
+def cmd_liste(param):
+    """Liste toutes les performances."""
+    print("Prénom      |  nage   |  longueur | date")
+    print("-----------------------------------------")
+    for elt in param['bdd']:
+        nageur = get_str_from_num_in_list(elt[0], param['nageurs'])
+        nage = get_str_from_num_in_list(elt[1], param['nages'])
+        print(f" {nageur:11}| {nage:8}|  {elt[2]:8}| {elt[3]}")
+
+
+def cmd_nageur(param):
+    """Liste les performances d'un nageur."""
+    for elt in param['nageurs']:
+        print(f"{elt[0]:5} : {elt[1]}")
+    tmp = int(input("Quel numéro de nageur ? "))
+    print(f"Performances de {get_str_from_num_in_list(tmp, param['nageurs'])}")
+    print("  nage   | longueur | date")
+    print("---------------------------")
+    for elt in param['bdd']:
+        if elt[0] == tmp:
+            nage = get_str_from_num_in_list(elt[1], param['nages'])
+            print(f" {nage:8}| {elt[2]:8}| {elt[3]}")
+
+
+def cmd_par_date(param):
+    """Liste les performances d'une date spécifique."""
+    date = input("Entrez la date (YYYY-MM-DD) : ")
+    try:
+        datetime.strptime(date, '%Y-%m-%d')
+    except ValueError:
+        print("Date invalide. Utilisez le format YYYY-MM-DD.")
+        return
+    print(f"Performances pour la date {date}")
+    print("Nageur      |  nage   | longueur")
+    print("-------------------------------")
+    for elt in param['bdd']:
+        if elt[3] == date:
+            nageur = get_str_from_num_in_list(elt[0], param['nageurs'])
+            nage = get_str_from_num_in_list(elt[1], param['nages'])
+            print(f" {nageur:11}| {nage:8}| {elt[2]}")
+
+
+def cmd_save(param, filename='save.csv'):
+    """Sauvegarde des données."""
+    with open(filename, 'w') as fichier:
+        fichier.write('@ nageurs\n')
+        for elt in param['nageurs']:
+            fichier.write(f"{elt[0]},{elt[1]}\n")
+        fichier.write('@ nages\n')
+        for elt in param['nages']:
+            fichier.write(f"{elt[0]},{elt[1]}\n")
+        fichier.write('@ bdd\n')
+        for elt in param['bdd']:
+            fichier.write(f"{elt[0]},{elt[1]},{elt[2]},{elt[3]}\n")
+    print("Données sauvegardées.")
+
+
+def cmd_load(param, filename='save.csv'):
+    """Charge des données sauvegardées."""
+    reset(param)
+    key = ''
+    with open(filename, 'r') as fichier:
+        for line in fichier:
+            line = line.strip()
+            if line.startswith('#'):
+                continue
+            if line.startswith('@'):
+                key = line[2:]
+                continue
+            if not key:
+                continue
+            tmp = line.split(',')
+            if key == 'bdd':
+                tmp[0], tmp[1], tmp[2] = int(tmp[0]), int(tmp[1]), int(tmp[2])
+            elif key in ('nages', 'nageurs'):
+                tmp[0] = int(tmp[0])
+            param[key].append(tuple(tmp))
+    print("Données chargées avec succès.")
+
+
+def gestion_commande(param):
+    """Boucle principale de gestion des commandes."""
     while True:
         afficher_menu()
         choix = input("Entrez le numéro correspondant à votre choix : ")
         if choix == "1":
-            print("Ajout d'une performance en cours...")
-            # Ajouter la logique associée ici
+            cmd_ajout(param)
         elif choix == "2":
-            print("Ajout d'un individu en cours...")
-            # Ajouter la logique associée ici
+            cmd_individu(param)
         elif choix == "3":
-            print("Ajout d'une nouvelle nage en cours...")
-            # Ajouter la logique associée ici
+            cmd_nouvelle_nage(param)
         elif choix == "4":
-            print("Liste des performances :")
-            # Ajouter la logique associée ici
+            cmd_liste(param)
         elif choix == "5":
-            print("Liste des performances d'un nageur :")
-            # Ajouter la logique associée ici
+            cmd_nageur(param)
         elif choix == "6":
-            print("Liste des nageurs pratiquant une nage :")
-            # Ajouter la logique associée ici
-        elif choix == "7":
-            print("Sauvegarde des données utilisateurs...")
-            # Ajouter la logique associée ici
+            cmd_par_date(param)
         elif choix == "8":
-            print("Chargement des données utilisateurs...")
-            # Ajouter la logique associée ici
+            cmd_save(param)
+        elif choix == "9":
+            cmd_load(param)
         elif choix == "0":
-            print("Quitte le logiciel. À bientôt !")
+            print("Au revoir !")
             break
         else:
             print("Option invalide. Veuillez réessayer.")
 
-# Lancer le menu interactif
-gestion_commande()
-#
-#   Programme principal
-#
-isAlive = True
-if os.path.exists('save.backup'):
-    cmd_load(param, 'save.backup')
-while isAlive:
-    commande = get_cmd()
 
-    if commande == "1":
-        cmd_ajout(param)
-        continue
-    if commande == "2":
-        cmd_individu(param)
-        continue
-
-    if commande == "3":
-        cmd_nouvelle_nage(param)
-        continue
-
-    if commande == "4":
-        cmd_liste(param)
-        continue
-
-    if commande == "5":
-        cmd_nageur(param)
-        continue
-
-    if commande == "6":
-        cmd_nage(param)
-        continue
-
-    if commande == "7":
-        cmd_save(param)
-        continue
-
-    if commande == "8":
-        cmd_load(param)
-        continue
-
-    if commande == "0":
-        isAlive = cmd_exit(param)
-        continue
-
-    print(f"Commande {commande} inconnue")
+# Programme principal
+if __name__ == "__main__":
+    if os.path.exists('save.backup'):
+        cmd_load(param, 'save.backup')
+    gestion_commande(param)
